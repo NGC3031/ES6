dba58a6a34332da7eeda82ad1dc754e08dd1f34d
NGC3031 <scott.d@ieee.org>
2019-03-18 13:58:39 +1100

    updated

--- taggedTemplateLiteral.js
+++ taggedTemplateLiteral.js
@@ -1,10 +1,17 @@
-//Tagged Template literals
+//Tagged Template literals sort of obvious but shows how they work
 
 function tag(strings, ...substitutes) {
-    console.log(strings[0]);
-    console.log(substitutes[2]);
+    var str0 = strings[0];
+    var str1 = strings[1];
+    var sub1 = substitutes[0];
+    var sub2 = substitutes[1];
+    var sub3 = substitutes[2];
+    return `${str0}${sub1}${str1}${sub2} ${sub3}`; //Note the space
 }
 var firstName = "Peter";
 var lastName = "Parker";
 var otherName = "Spidey";
-tag `Hey there! It\'s you, ${firstName}, ${lastName}, ${otherName}`;
+
+var output = tag `The real name of ${otherName} is ${firstName} ${lastName}`;
+
+console.log(output);
