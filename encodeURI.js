5fe19fe100c902b420e5d831d216bdfa2e82857a
NGC3031 <scott.d@ieee.org>
2019-03-19 12:46:50 +1100

    new

--- encodeURI.js
+++ encodeURI.js
@@ -1,6 +1,13 @@
 // Encode and decode URIS - does what is says on the tin
 
-var url='https://uvw.io/somescript?dont bother';
-console.log(encodeURI(url));
+var url = 'https://uvw.io/somescript?dont bother';
+var a = encodeURIComponent(url);
+var b = decodeURIComponent(a);
+var c = encodeURIComponent(url);
+var d = decodeURIComponent(c)
 
-console.log(encodeURIComponent(url));
+console.log(a);
+console.log(b);
+
+console.log(c);
+console.log(d);
