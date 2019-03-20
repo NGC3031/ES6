44f3583a61fb32b525002f0883c55344dea82cfc
NGC3031 <scott.d@ieee.org>
2019-03-20 17:31:22 +1100

    ES6 cool stuff

---
+++ arrayDestructuring.js
@@ -0,0 +1,19 @@
+//Array destructuring
+
+//instead of 
+//let x = 1;
+//let y = 2;
+//have some syntactic sugar
+let [x, y] = [1, 2];
+console.log(x);
+//actually this is really good for you also
+//instead of:
+var temp = 4;
+x = y;
+y = temp;
+//save the namespace et' voila
+[x, y] = [1, 2];
+console.log(x);
+[x, y] = [y, x];
+console.log(`This is the new ${x}`);
+//neat!!
